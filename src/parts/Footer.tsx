85e6af0af408edd20cf462bab47249a8ea51cb49
chocolate-kuma <natsumi0814okada@gmail.com>
2023-01-22 02:02:47 +0900

    fix footer

--- src/parts/Footer.tsx
+++ src/parts/Footer.tsx
@@ -1,22 +1,41 @@
-import { Box } from "@chakra-ui/react";
-import React from "react";
+import { Box, Flex, Text, Image, Spacer } from "@chakra-ui/react";
+import React, { useState } from "react";
+import ProkenIcon from "../components/ProkenIcon";
+import proken_icon from "../images/proken_icon.jpeg";
+import Navigator from "../components/Navigator";
+import TwitterIcon from "../images/twitter_icon.jpeg"
+
 
 const Footer = () => {
+  const twitter = () => {
+    window.open("https://twitter.com/kyutech_proken");
+  };
+  const [position] = useState<any>("absolute");
   return (
     <Box
-      backgroundColor="#4FFFFF"
+      backgroundColor="#FFFFFF"
       borderTop="solid 1px"
       left="0"
       bottom="0"
       width="100%"
     >
-      <Box
-        className="footer_menu"
-        height="200px"
-        
+      <Flex
+        className="inner"
+        width="100%"
+        height="80px"
+        position={position}
+        borderBottom="solid"
+        alignItems="center"
       >
-        <Box>footer</Box>
-      </Box>
+        <Box width="24px"></Box>
+        <Image src={proken_icon} width="56px" height="56px"></Image>
+        <Box width="24px"></Box>
+        <ProkenIcon />
+        <Navigator />
+        <Spacer />
+        <Image src={TwitterIcon} width="56px" height="56px" onClick={twitter}></Image>
+        <Box width="24px"></Box>
+      </Flex>
     </Box>
   );
 };
