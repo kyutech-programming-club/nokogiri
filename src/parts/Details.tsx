d2382f7e783f6058d0eaf0350a8aa94602f076b6
shotaro-ada <syouta2223505@gmail.com>
2023-01-21 10:06:06 +0900

    dd

---
+++ src/parts/Details.tsx
@@ -0,0 +1,15 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+import Staffs from "../components/Staffs";
+import Title from "../components/Title";
+
+const Details = () => {
+  return (
+    <Box>
+      <Title />
+      <Staffs />
+    </Box>
+  );
+};
+
+export default Details;
