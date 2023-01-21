cd4ee4c5a3fee6212b0bc847c65f66e3ca5952cc
chocolate-kuma <natsumi0814okada@gmail.com>
2023-01-21 22:09:39 +0900

    add slider

--- src/templates/HomeSlider.tsx
+++ src/templates/HomeSlider.tsx
@@ -1,14 +1,29 @@
-import { Box } from "@chakra-ui/react";
+import { Box} from "@chakra-ui/react";
 import React from "react";
-import pic from '../images/25513290_s.jpg'
+import pic from "../images/25513290_s.jpg";
+import "./HomeSlider.css";
 
 const HomeSlider = () => {
+  const BoxClick1 = () =>{
+    window.open('https://qiita.com/proken/items/80d20adcf24b2b53c149')
+  };
   return (
-    <Box backgroundImage={pic} >
-      <Box>
-        <Box width="100%" height="480px"></Box>
+    <>
+      <Box backgroundImage={pic}>
+        <Box>
+          <Box width="100%" height="480px">
+            <ul className="horizontal-list">
+              <li className="item"><Box onClick={BoxClick1}>環境</Box></li>
+              <li className="item"><Box>aaa</Box></li>
+              <li className="item"><Box>aaa</Box></li>
+              <li className="item"><Box>aaa</Box></li>
+              <li className="item"><Box>aaa</Box></li>
+              <li className="item"><Box>aaa</Box></li>
+            </ul>
+          </Box>
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
 
