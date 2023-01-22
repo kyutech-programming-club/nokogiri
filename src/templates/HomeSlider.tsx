9ba206d2f47709474601d88f8d5077c264ff8fec
chocolate-kuma <natsumi0814okada@gmail.com>
2023-01-22 12:57:00 +0900

    delete box

--- src/templates/HomeSlider.tsx
+++ src/templates/HomeSlider.tsx
@@ -10,7 +10,6 @@ const HomeSlider = () => {
   return (
     <>
       <Box backgroundImage={pic}>
-        <Box>
           <Box width="100%" height="480px">
             <ul className="horizontal-list">
               <li className="item"><Box onClick={BoxClick1}>環境</Box></li>
@@ -21,7 +20,6 @@ const HomeSlider = () => {
               <li className="item"><Box>aaa</Box></li>
             </ul>
           </Box>
-        </Box>
       </Box>
     </>
   );
